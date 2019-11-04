ba5dbd403a4c7e3ebe712e8e6fce052e2b41baf1
Greg <gkmarsella@gmail.com>
2019-11-03 21:40:21 -0500

    Refactoring and adding tests

--- polls/models.py
+++ polls/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 # Create your models here.
 
+import datetime
 from django.db import models
 from django.utils import timezone
 
@@ -14,7 +15,8 @@ class Question(models.Model):
     def __str__(self):
         return self.question_text
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
 
 class Choice(models.Model):
@@ -24,4 +26,5 @@ class Choice(models.Model):
     # for Question.objects.all()
     def __str__(self):
         return self.choice_text
+
     
